af91a9ad8ec9aefaeadca119e5858c019a424989
VernandaFazira <145659644+VernandaFazira@users.noreply.github.com>
2026-07-28 15:27:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,23 @@
 const CACHE_NAME = "dzikirku-v1";
 
-const urls = [
-    "./",
-    "./index.html"
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urls))
-    );
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
